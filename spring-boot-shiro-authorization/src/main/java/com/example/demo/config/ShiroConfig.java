931612bd97116dcc8fef1365f9d4373a2f9b29cc
SpringLych <2571568860@qq.com>
2019-08-03 14:37:40 +0800

    添加注释

--- spring-boot-shiro-authorization/src/main/java/com/example/demo/config/ShiroConfig.java
+++ spring-boot-shiro-authorization/src/main/java/com/example/demo/config/ShiroConfig.java
@@ -15,7 +15,7 @@ import java.util.LinkedHashMap;
 /**
  * @author LiYingChun
  * @date 2019/8/2
- * 成功版本
+ * Shiro配置类
  */
 @Configuration
 public class ShiroConfig {
@@ -23,12 +23,15 @@ public class ShiroConfig {
     @Bean
     public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+
+        //设置securityManager
         shiroFilterFactoryBean.setSecurityManager(securityManager);
         shiroFilterFactoryBean.setLoginUrl("/login");
         shiroFilterFactoryBean.setSuccessUrl("/index");
         shiroFilterFactoryBean.setUnauthorizedUrl("/403");
         LinkedHashMap<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
 
+        //authc表示需要验证身份才能访问，还有一些比如anon表示不需要验证身份就能访问等。
         filterChainDefinitionMap.put("/css/**", "anon");
         filterChainDefinitionMap.put("/js/**", "anon");
         filterChainDefinitionMap.put("/fonts/**", "anon");
@@ -46,6 +49,7 @@ public class ShiroConfig {
     @Bean
     public SecurityManager securityManager() {
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        // 配置securityManager，注入shiroRealm
         securityManager.setRealm(shiroRealm());
         securityManager.setRememberMeManager(rememberMeManager());
         return securityManager;
@@ -58,12 +62,18 @@ public class ShiroConfig {
         return shiroRealm;
     }
 
+    /**
+     * rememberMeCookie，重开浏览器后无需重新登录
+     */
     public SimpleCookie rememberMeCookie() {
         SimpleCookie cookie = new SimpleCookie("rememberMe");
         cookie.setMaxAge(86400);
         return cookie;
     }
 
+    /**
+     * cookie 管理对象
+     */
     public CookieRememberMeManager rememberMeManager() {
         CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
         cookieRememberMeManager.setCookie(rememberMeCookie());
@@ -71,6 +81,10 @@ public class ShiroConfig {
         return cookieRememberMeManager;
     }
 
+    /**
+     * 开启shiro aop注解支持.使用代理方式;所以需要开启代码支持;
+     * 只有开启了AOP才执行doGetAuthorizationInfo()，也就权限拦截
+     */
     @Bean
     public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
         AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
