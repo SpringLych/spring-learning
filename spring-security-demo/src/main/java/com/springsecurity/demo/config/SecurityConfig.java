aab261551c56d448b4f5a060dd7c7f9d77aa0618
SpringLych <liyingchuncn@163.com>
2020-10-10 17:50:22 +0800

    feat: add json formatting

--- spring-security-demo/src/main/java/com/springsecurity/demo/config/SecurityConfig.java
+++ spring-security-demo/src/main/java/com/springsecurity/demo/config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.springsecurity.demo.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.io.PrintWriter;
 
 /**
  * @author SpringLych
@@ -40,5 +44,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         manager.createUser(User.withUsername("admin").password("123").roles("admin").build());
         manager.createUser(User.withUsername("test").password("123").roles("test").build());
         return manager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/doLogin")
+                .successHandler((req, resp, authentication) -> {
+                    Object principal = authentication.getPrincipal();
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString(principal));
+                    out.flush();
+                    out.close();
+                })
+                .failureHandler((req, resp, e) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(e.getMessage());
+                    out.flush();
+                    out.close();
+                })
+                .and()
+                .logout()
+                .logoutSuccessHandler((req, resp, authentication) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write("注销成功");
+                    out.flush();
+                    out.close();
+                })
+                .and()
+                .csrf().disable().exceptionHandling()
+                .authenticationEntryPoint((req, resp, authException) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write("尚未登录，请先登录");
+                    out.flush();
+                    out.close();
+                });
     }
 }
